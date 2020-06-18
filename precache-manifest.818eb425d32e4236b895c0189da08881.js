9a098df50e23dfa848c70b131de01b78e22c68f7
Jacopo <jacomig@gmail.com>
2020-06-18 13:40:44 +0200

    Updates

---
+++ precache-manifest.818eb425d32e4236b895c0189da08881.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1568445ef6175a484475986c916f1f0d",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "b084fcc52fa7616c8480",
+    "url": "/drum-machine/static/css/2.74512e07.chunk.css"
+  },
+  {
+    "revision": "02bc6d8480ced4378125",
+    "url": "/drum-machine/static/css/main.584c9104.chunk.css"
+  },
+  {
+    "revision": "b084fcc52fa7616c8480",
+    "url": "/drum-machine/static/js/2.fd9e5a74.chunk.js"
+  },
+  {
+    "revision": "af8b6d3dfaeca0c6db42a14ce19238aa",
+    "url": "/drum-machine/static/js/2.fd9e5a74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02bc6d8480ced4378125",
+    "url": "/drum-machine/static/js/main.bae533db.chunk.js"
+  },
+  {
+    "revision": "fc1b3e84514a8c25bf54",
+    "url": "/drum-machine/static/js/runtime-main.1e07d996.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/drum-machine/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/drum-machine/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/drum-machine/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/drum-machine/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/drum-machine/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
