ca0f82538ef0e64d7265fcb471614f29d2734a6d
Jacopo <jacomig@gmail.com>
2020-06-18 13:47:50 +0200

    Add Title and Text

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <DrumMachine />
+      <p>Drum Machine made with <strong>Tonejs</strong> and <strong>React</strong></p>
     </div>
   );
 }
